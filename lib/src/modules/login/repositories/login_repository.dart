42a0316582d52e3703ed9ca30830e12db52017ec
Saulo <saulomiqueias7@gmail.com>
2021-12-11 03:01:33 -0300

    refactor

--- lib/src/modules/login/repositories/login_repository.dart
+++ lib/src/modules/login/repositories/login_repository.dart
@@ -1,7 +1,8 @@
+import 'package:tacaro_app/src/core/models/user_model.dart';
 import 'package:tacaro_app/src/core/services/app_database.dart';
 
 abstract class LoginRepository {
-  Future<bool> login({required String email, required String password});
+  Future<UserModel> login({required String email, required String password});
 }
 
 class LoginRepositoryImpl implements LoginRepository {
@@ -9,7 +10,7 @@ class LoginRepositoryImpl implements LoginRepository {
   LoginRepositoryImpl({required this.database});
 
   @override
-  Future<bool> login({required String email, required String password}) async {
+  Future<UserModel> login({required String email, required String password}) async {
     final response = await database.login(email: email, password: password);
     return response;
   }
